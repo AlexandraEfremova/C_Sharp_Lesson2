bc54ca4f179867feb8ce738cc2690864dfdead3d
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-01 13:49:31 +0300

    lesson2 : Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = 10;
+int[] array = { 2, 24, 53, 2, 64, 74, 4, 7, 1, 11 };
+int i = 0;
+while(i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
