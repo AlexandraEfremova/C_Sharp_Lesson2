08ded6126b7f9bc9e4af882a4d6e8058c7306573
Efremova Aleksandra <doctor.akab@gmail.com>
2024-08-01 12:34:21 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Создади массив на 5 целых чисел
+int[] arr = new int[5];
+// Присвоим новыеи значения элементам массива
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+// Для проверки массива выведем элементы на экран
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
